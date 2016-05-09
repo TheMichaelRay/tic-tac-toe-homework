5d7640d58e01813a647d8d7f0d4cdf778b5f2e62
Michael Ray <raymichael23@gmail.com>
2016-05-08 18:09:22 -0700

    added win logic for all rows, columns, and diagonals

--- app.js
+++ app.js
@@ -8,14 +8,33 @@ var game = {
 }
 
 var currentPlayer = game.player1;
-
+var mark = currentPlayer.marker;
 var winner;
 
 function winRow() {
-  if (squares[0].innerHTML === squares[1].innerHTML && squares[0].innerHTML === squares[2].innerHTML) {
-    console.log("BOOM");
-    winner = currentPlayer
+  if ((squares[0].innerHTML === mark && squares[1].innerHTML === mark && squares[2].innerHTML === mark) ||
+      (squares[3].innerHTML === mark && squares[4].innerHTML === mark && squares[5].innerHTML === mark) ||
+      (squares[6].innerHTML === mark && squares[7].innerHTML === mark && squares[8].innerHTML === mark)) {
+        console.log("BOOM");
+        winner = currentPlayer
   }
+}
+
+function winColumn() {
+  if ((squares[0].innerHTML === mark && squares[3].innerHTML === mark && squares[6].innerHTML === mark) ||
+      (squares[1].innerHTML === mark && squares[4].innerHTML === mark && squares[7].innerHTML === mark) ||
+      (squares[2].innerHTML === mark && squares[5].innerHTML === mark && squares[8].innerHTML === mark)) {
+        console.log("BOOM");
+        winner = currentPlayer
+  }
+}
+
+function winDiag() {
+  if ((squares[0].innerHTML === mark && squares[4].innerHTML === mark && squares[8].innerHTML === mark) ||
+      (squares[2].innerHTML === mark && squares[4].innerHTML === mark && squares[6].innerHTML === mark)) {
+        console.log("BOOM");
+        winner = currentPlayer;
+      }
 }
 
 function switchTurns() {
@@ -34,16 +53,18 @@ var squares = document.querySelectorAll('.square');
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', function(){
-      if (winner){
-        console.log(currentPlayer + " has won!")
-      } else if (!this.innerHTML ) {
+      // if (winner){
+        // console.log(currentPlayer + " has won!")} else
+        if (!this.innerHTML && !winner) {
         console.log(this);
-        this.innerHTML = "<span>" + currentPlayer.marker + "</span>";
+        this.innerHTML = currentPlayer.marker;
         // check if the square has already been marked
         // mark an x or an o
         // check to see if there's a winner
         // lots of action taking place in here
         winRow();
+        winColumn();
+        winDiag();
         switchTurns();
       }
       })
