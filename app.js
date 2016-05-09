d25fb88ba591f2dac55eea0f708587cd10a34548
Michael Ray <raymichael23@gmail.com>
2016-05-08 23:39:48 -0700

    added reset button, colors, and backgrounds for each player

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var game = {
   player1: {
     marker: "X",
     score: 0,
-    html: '#playerXScore'
+    html: '#playerXScore',
+    class: ' red'
   },
   player2: {
     marker: "O",
     score: 0,
-    html: '#playerOScore'
+    html: '#playerOScore',
+    class: ' blue'
   }
 }
 
@@ -15,14 +17,24 @@ var currentPlayer = game.player1;
 var mark = currentPlayer.marker;
 var winner;
 var squares = document.querySelectorAll('.square');
-var scores = document.querySelector(currentPlayer.html);
 // var playerOScore = document.querySelector('#playerOScore');
 // var playerXScore = document.querySelector('#playerXScore');
+function reset() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].innerHTML = '';
+    squares[i].classList = 'square';
+  }
+}
+
+
 function logWinner(){
   console.log("BOOM");
-  winner = currentPlayer;
+  // winner = currentPlayer;
+  alert(currentPlayer.marker + " has won!")
   currentPlayer.score +=1;
-  scores.innerHTML = currentPlayer.score
+  document.querySelector(currentPlayer.html).innerHTML = currentPlayer.score;
+  // winner = null;
+  reset();
 }
 
 function getWinner (){
@@ -57,9 +69,11 @@ function winDiag() {
 
 function switchTurns() {
   if (currentPlayer == game.player1){
-    currentPlayer = game.player2
+    currentPlayer = game.player2;
+    mark = currentPlayer.marker
   } else {
-    currentPlayer = game.player1
+    currentPlayer = game.player1;
+    mark = currentPlayer.marker
   }
 }
 
@@ -67,22 +81,23 @@ function switchTurns() {
 // squares.addEventListener('click', function(){
 //   console.log("clicked!!")
 // })
+document.querySelector('button').addEventListener('click', reset)
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', function(){
       // if (winner){
         // console.log(currentPlayer + " has won!")} else
-        if (!this.innerHTML && !winner) {
+        if (!this.innerHTML) {
         console.log(this);
+        this.classList += currentPlayer.class;
         this.innerHTML = currentPlayer.marker;
         // check if the square has already been marked
         // mark an x or an o
         // check to see if there's a winner
         // lots of action taking place in here
-        winRow();
-        winColumn();
-        winDiag();
+        getWinner();
         switchTurns();
       }
-      })
+      }
+      )
     }
