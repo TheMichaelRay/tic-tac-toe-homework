151dc9bfadba6569e3abdb5464e80848c5252831
Michael Ray <raymichael23@gmail.com>
2016-05-11 16:29:32 -0700

    saved changes for deleting commented out code"

--- app.js
+++ app.js
@@ -17,9 +17,6 @@ var currentPlayer = game.player1;
 var mark = currentPlayer.marker;
 var winner;
 var squares = document.querySelectorAll('.square');
-// document.querySelector()
-// var playerOScore = document.querySelector('#playerOScore');
-// var playerXScore = document.querySelector('#playerXScore');
 function reset() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].innerHTML = '';
@@ -30,11 +27,9 @@ function reset() {
 
 function logWinner(){
   console.log("BOOM");
-  // winner = currentPlayer;
   alert(currentPlayer.marker + " has won!")
   currentPlayer.score +=1;
   document.querySelector(currentPlayer.html).innerHTML = currentPlayer.score;
-  // winner = null;
   reset();
 }
 
@@ -55,22 +50,6 @@ function winRow() {
         logWinner()
   }
 }
-// function winRow() {
-//   if ((squares[0].innerHTML === mark && squares[1].innerHTML === mark && squares[2].innerHTML === mark) ||
-//       (squares[3].innerHTML === mark && squares[4].innerHTML === mark && squares[5].innerHTML === mark) ||
-//       (squares[6].innerHTML === mark && squares[7].innerHTML === mark && squares[8].innerHTML === mark)) {
-//         logWinner()
-//   }
-// }
-
-
-// function winColumn() {
-//   if ((squares[0].innerHTML === mark && squares[3].innerHTML === mark && squares[6].innerHTML === mark) ||
-//       (squares[1].innerHTML === mark && squares[4].innerHTML === mark && squares[7].innerHTML === mark) ||
-//       (squares[2].innerHTML === mark && squares[5].innerHTML === mark && squares[8].innerHTML === mark)) {
-//         logWinner()
-//   }
-// }
   function winColumn() {
     for (i=0; i<3; i++) {
       if (squares[i].innerHTML === mark && squares[i+3].innerHTML === mark && squares[i+6].innerHTML === mark) {
@@ -99,23 +78,14 @@ function switchTurns() {
 }
 
 
-// squares.addEventListener('click', function(){
-//   console.log("clicked!!")
-// })
 document.querySelector('button').addEventListener('click', reset)
 
 for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', function(){
-    // if (winner){
-    // console.log(currentPlayer + " has won!")} else
     if (!this.innerHTML) {
       console.log(this);
       this.classList += currentPlayer.class;
       this.innerHTML = currentPlayer.marker;
-      // check if the square has already been marked
-      // mark an x or an o
-      // check to see if there's a winner
-      // lots of action taking place in here
       getWinner();
       switchTurns();
     }
