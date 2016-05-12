02ba2ac8564c678b8b2f2377ba0c03691eee1fe0
Michael Ray <raymichael23@gmail.com>
2016-05-11 18:49:18 -0700

    game should be working as intended for now

--- app.js
+++ app.js
@@ -12,75 +12,77 @@ var game = {
     html: '#playerOScore',
     class: ' blue'
   },
-  // currentPlayer: game.player1,
-  // mark: game.currentPlayer.marker,
-  $squares: $('.square')
-  // // function bank
-  // functions: {
-  //   logWinner: function(){
-  //     console.log("BOOM");
-  //     alert(game.currentPlayer.marker + " has won!")
-  //     game.currentPlayer.score +=1;
-  //     $(game.currentPlayer.html).html(game.currentPlayer.score);
-  //     game.reset();
-  //   },
-  // },
-  // reset: function() {
-  //     game.$squares.html('');
-  //     game.$squares.removeClass('red blue');
-  // },
-  // getWinner: function(){
-  //   game.functions.winRow();
-  //   game.functions.winColumn();
-  //   game.functions.winDiag();
-  //    if ($('.red').length + $('.blue').length === game.$squares.length){
-  //     alert("Cat's Game :3");
-  //     game.functions.reset();
-  //   }
-  // },
-  // winRow: function() {
-  //   for(i=0; i<9; i+=3) {
-  //   if ($(game.$squares[i]).html() === game.mark && $(game.$squares[i+1]).html() === game.mark && $(game.$squares[i+2]).html() === game.mark) {
-  //         game.functions.logWinner()
-  //       }
-  //   }
-  // },
-  // winColumn: function() {
-  //   for (i=0; i<3; i++) {
-  //     if ($(game.$squares[i]).html() === game.mark && $(game.$squares[i+3]).html() === game.mark && $(game.$squares[i+6]).html() === game.mark) {
-  //         game.functions.logWinner()
-  //         }
-  //     }
-  // },
-  // winDiag: function() {
-  //   if (($(game.$squares[0]).html() === game.mark && $(game.$squares[4]).html() === game.mark && $(game.$squares[8]).html() === game.mark) ||
-  //       ($(game.$squares[2]).html() === game.mark && $(game.$squares[4]).html() === game.mark && $(game.$squares[6]).html() === game.mark)) {
-  //         game.functions.logWinner()
-  //       }
-  // },
-  // switchTurns: function() {
-  //   if (game.currentPlayer == game.player1){
-  //     game.currentPlayer = game.player2;
-  //     $('#currentPlayerMove').html(currentPlayer.marker);
-  //     game.mark = currentPlayer.marker
-  //   } else {
-  //     game.currentPlayer = game.player1;
-  //     $('#currentPlayerMove').html(currentPlayer.marker);
-  //     game.mark = currentPlayer.marker
-  //   }
-  // },
-  // init: function(){
-  //   $('button').on('click', game.reset);
-  //   $squares.on('click', function(){
-  //     if (!$(this).html()) {
-  //       console.log(this);
-  //       $(this).addClass(currentPlayer.class);
-  //       $(this).html(currentPlayer.marker);
-  //       game.functions.getWinner();
-  //       game.functions.switchTurns();
-  //       }
-  //     })
-  // }
+  currentPlayer: null,
+  mark: null,
+  $squares: $('.square'),
+  // function bank
+  functions: {
+    logWinner: function(){
+      console.log("BOOM");
+      alert(game.currentPlayer.marker + " has won!")
+      game.currentPlayer.score +=1;
+      $(game.currentPlayer.html).html(game.currentPlayer.score);
+      game.functions.reset();
+    },
+    reset: function() {
+      game.$squares.html('');
+      game.$squares.removeClass('red blue');
+    },
+    getWinner: function(){
+    game.functions.winRow();
+    game.functions.winColumn();
+    game.functions.winDiag();
+     if ($('.red').length + $('.blue').length === game.$squares.length){
+      alert("Cat's Game :3");
+      game.functions.reset();
+      }
+    },
+    winRow: function() {
+    for(i=0; i<9; i+=3) {
+    if ($(game.$squares[i]).html() === game.mark && $(game.$squares[i+1]).html() === game.mark && $(game.$squares[i+2]).html() === game.mark) {
+          game.functions.logWinner()
+        }
+      }
+    },
+    winColumn: function() {
+    for (i=0; i<3; i++) {
+      if ($(game.$squares[i]).html() === game.mark && $(game.$squares[i+3]).html() === game.mark && $(game.$squares[i+6]).html() === game.mark) {
+          game.functions.logWinner()
+          }
+      }
+    },
+    winDiag: function() {
+      if (($(game.$squares[0]).html() === game.mark && $(game.$squares[4]).html() === game.mark && $(game.$squares[8]).html() === game.mark) ||
+          ($(game.$squares[2]).html() === game.mark && $(game.$squares[4]).html() === game.mark && $(game.$squares[6]).html() === game.mark)) {
+            game.functions.logWinner()
+          }
+    },
+    switchTurns: function() {
+      if (game.currentPlayer == game.player1){
+        game.currentPlayer = game.player2;
+        $('#currentPlayerMove').html(game.currentPlayer.marker);
+        game.mark = game.currentPlayer.marker
+      } else {
+        game.currentPlayer = game.player1;
+        $('#currentPlayerMove').html(game.currentPlayer.marker);
+        game.mark = game.currentPlayer.marker
+        }
+      }
+  },
+  init: function(){
+    $('button').on('click', game.reset);
+    this.currentPlayer = game.player1;
+    this.mark = game.currentPlayer.marker;
+    $('.square').on('click', function(){
+      if (!$(this).html()) {
+        console.log(this);
+        $(this).addClass(game.currentPlayer.class);
+        $(this).html(game.currentPlayer.marker);
+        game.functions.getWinner();
+        game.functions.switchTurns();
+        }
+      })
+  }
 };
 
-// game.init()
+game.init()
